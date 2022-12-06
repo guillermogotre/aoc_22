fdafcc212d9fab109c4d7ba45b681a795c2cfb51
Guillermo <guillermogtrenado@gmail.com>
2022-12-06 11:40:48 +0100

    init files for new day

---
+++ new_day.py
@@ -0,0 +1,7 @@
+import glob
+import os
+from pathlib import Path
+next = max(int(os.path.splitext(x)[0]) for x in glob.glob("*.ipynb")) + 1
+
+for f in [f'{next}.ipynb', f'{next}.input', f'{next}_ex.input']:
+    Path(f).touch()
