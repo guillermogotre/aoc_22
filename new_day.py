bb8f32b7771ed8853b29ec7c8043c23ade0a29fe
Guillermo <guillermogtrenado@gmail.com>
2022-12-07 12:50:11 +0100

    move inputs

--- new_day.py
+++ new_day.py
@@ -3,5 +3,5 @@ import os
 from pathlib import Path
 
 next = max(int(os.path.splitext(x)[0]) for x in glob.glob("*.ipynb")) + 1
-for f in [f'{next}.ipynb', f'{next}.input', f'{next}_ex.input']:
+for f in [f'{next:02d}.ipynb', f'inputs/{next:02d}.input', f'inputs/{next:02d}_ex.input']:
     Path(f).touch()
